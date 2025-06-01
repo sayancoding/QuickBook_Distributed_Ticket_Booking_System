756d39cb2f689c65595efa11b232d4ff7c7c6be1
sayancoding <sayanmaity007@gmail.com>
2025-06-01 10:59:00 +0530

    venue service init

---
+++ venue-service/src/main/java/com/example/venue_service/VenueServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.venue_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenueServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VenueServiceApplication.class, args);
+    }
+}
